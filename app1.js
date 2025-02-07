7d64714188723ad59a43bccdd6929d7cbab25ea2
rajan0031 <109027109+rajan0031@users.noreply.github.com>
2025-02-07 16:40:14 +0530

    added the app.js file

---
+++ app1.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hello from the backend");
+})
+
+app.listen(3001, () => {
+    console.log("server is listening at the port ", 3001)
+})
